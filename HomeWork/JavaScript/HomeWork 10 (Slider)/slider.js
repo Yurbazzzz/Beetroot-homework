c16df28106abee58575b0733afeee361e6c8ee6e
Yurbazzzz <74986111+Yurbazzzz@users.noreply.github.com>
2021-03-08 23:48:07 +0200

    Slider

---
+++ HomeWork/JavaScript/HomeWork 10 (Slider)/slider.js
@@ -0,0 +1,11 @@
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: false,
+
+    nextArrow: '.slick-next',
+    prevArrow: '.slick-prev',
+  });
